276b4ec763c3d9142897b0108b719b00daa48c9c
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-09-23 20:33:58 +0100

    Done.

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,21 +6,11 @@
 /*   By: abait-el <abait-el@example.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/23 15:50:10 by abait-el          #+#    #+#             */
-/*   Updated: 2025/09/23 16:59:40 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/09/23 20:33:39 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-static int	ft_islowcase(int c)
-{
-	return (c >= 'a' && c <= 'z');
-}
-
-static int	ft_isupcase(int c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
-
 int	ft_isalpha(int c)
 {
-	return (ft_islowcase(c) || ft_isupcase(c));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
