6983a25e67f071440d798942142dfebf991411f8
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-10-17 17:37:18 +0100

    Remove test folder

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 22:19:44 by abait-el          #+#    #+#             */
-/*   Updated: 2025/10/17 12:44:34 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/10/17 17:31:33 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,29 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dstlen = ft_strlen(dst);
 	dst += ft_strlen(dst);
 	i = dstlen;
-	while (i < (size_t)(dst - (dstlen + 1)))
+	while (i < (dstsize - (dstlen + 1)))
 	{
 		dst[i] = src[i - dstlen];
 		i++;
 	}
 	dst[i] = '\0';
 	return (dstlen + ft_strlen(src));
+}
+
+
+#include <stdio.h>
+#include "libft.h"
+
+int main(void)
+{
+    char dst[20] = "Hello";
+    const char *src = " World!";
+    size_t ret;
+
+    ret = ft_strlcat(dst, src, sizeof(dst));
+
+    printf("Resulting string : \"%s\"\n", dst);
+    printf("Returned length  : %zu\n", ret);
+
+    return 0;
 }
--- tests/ft_strtrim/test.c
+++
@@ -1,6 +0,0 @@
-int	main(void)
-{
-	char	*str = " \tHello\tWorld \t ";
-#include <stdio.h>
-	printf("%s\n", ft_strtrim(str, " \t"));
-}
